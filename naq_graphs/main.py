8b7ee3ed284a264206155850eaada78c9c5ea827
alexis arnaudon <alexis.arnaudon@epfl.ch>
2020-02-07 08:39:59 +0100

    graph_construction file

--- naq_graphs/main.py
+++ naq_graphs/main.py
@@ -4,6 +4,7 @@ import multiprocessing
 import numpy as np
 import scipy as sc
 
+from .graph_construction import construct_laplacian
 
 class WorkerScan:
     """worker to scan complex frequency"""
@@ -35,6 +36,7 @@ def scan_frequencies(graph, dispersion_relation, params, n_workers=1):
     qualities = sc.sparse.coo_matrix(
         (qualities_list, (id_k, id_a)), shape=(params["k_n"], params["alpha_n"])
     ).toarray()
+
     return ks, alphas, qualities
 
 
@@ -52,85 +54,4 @@ def mode_quality(laplacian, method="eigenvalue"):
 
     return 0.
 
-def construct_laplacian(freq, graph, dispersion_relation):
-    """construct naq laplacian from a graph"""
-    set_wavenumber(graph, freq, dispersion_relation)
-    BT, Bout = construct_incidence_matrix(graph)
-    Winv = construct_weight_matrix(graph)
-    return BT.dot(Winv).dot(Bout)
 
-
-def set_wavenumber(graph, freq, dispersion_relation):
-    """set edge wavenumbers from frequency and dispersion relation"""
-    for ei, e in enumerate(list(graph.edges())):
-        graph[e[0]][e[1]]["k"] = dispersion_relation(freq, ei)
-
-
-def construct_incidence_matrix(graph):
-    """Construct the incidence matrix B"""
-    row = []
-    col = []
-    data_B = []
-    data_Bout = []
-    for ei, e in enumerate(list(graph.edges())):
-        (u, v) = e[:2]
-
-        expl = np.exp(1.0j * graph[u][v]["length"] * graph[u][v]["k"])
-
-        row.append(2 * ei)
-        col.append(u)
-        data_B.append(-1)
-        data_Bout.append(-1)
-
-        row.append(2 * ei)
-        col.append(v)
-        data_B.append(expl)
-        if len(graph[u]) == 1 or len(graph[v]) == 1:
-            data_Bout.append(0)
-        else:
-            data_Bout.append(expl)
-
-        row.append(2 * ei + 1)
-        col.append(u)
-        data_B.append(expl)
-        if len(graph[u]) == 1 or len(graph[v]) == 1:
-            data_Bout.append(0)
-        else:
-            data_Bout.append(expl)
-
-        row.append(2 * ei + 1)
-        col.append(v)
-        data_B.append(-1)
-        data_Bout.append(-1)
-
-    m = len(graph.edges)
-    n = len(graph.nodes)
-    B = sc.sparse.coo_matrix((data_B, (row, col)), shape=(2 * m, n))
-    Bout = sc.sparse.coo_matrix((data_Bout, (row, col)), shape=(2 * m, n))
-
-    return B.T.asformat("csc"), Bout.asformat("csc")
-
-
-def construct_weight_matrix(graph):
-    """Construct the matrix W^{-1}"""
-    row = []
-    data = []
-    for ei, e in enumerate(list(graph.edges())):
-        (u, v) = e[:2]
-
-        if abs(graph[u][v]["k"]) > 0:
-            w = graph[u][v]["k"] / (
-                np.exp(2.0 * graph[u][v]["length"] * graph[u][v]["k"]) - 1.0
-            )
-        else:
-            w = -0.5 * graph[u][v]["length"]
-
-        row.append(2 * ei)
-        row.append(2 * ei + 1)
-        data.append(w)
-        data.append(w)
-
-    m = len(graph.edges)
-    return sc.sparse.coo_matrix((data, (row, row)), shape=(2 * m, 2 * m)).asformat(
-        "csc"
-    )
