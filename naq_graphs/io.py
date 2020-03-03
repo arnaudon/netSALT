91e3e8d45af0a11246a57d88cd10151a75811b60
Alexis Arnaudon <alexis.arnaudon@epfl.ch>
2020-03-03 17:01:15 +0100

    small bug

--- naq_graphs/io.py
+++ naq_graphs/io.py
@@ -15,7 +15,7 @@ def load_graph(filename="graph.pkl"):
 def save_modes(modes, lasing_thresholds=None, filename="passive_modes"):
     """save modes in a pickle"""
     if lasing_thresholds is None:
-        pickle.dump(modes, open(filename, "wb"))
+        pickle.dump(modes, open(filename + ".pkl", "wb"))
     else:
         pickle.dump([modes, lasing_thresholds], open(filename + ".pkl", "wb"))
 
