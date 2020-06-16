cf4b1c50dee707a2f8b45585a52e1e28709f04fa
alexis arnaudon <alexis.arnaudon@epfl.ch>
2020-06-16 22:21:54 +0200

    modal intensity merge from dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 if sys.version_info < (2, 7):
     sys.exit("Sorry, Python < 2.7 is not supported")
 
-VERSION = '0.1.0'
+VERSION = "0.1.0"
 
 setup(
     name="naq-graphs",
@@ -17,11 +17,11 @@ setup(
     version=VERSION,
     description="",
     install_requires=[
-        'scipy>=1.2.0',
-        'networkx',
-        'matplotlib',
-        'scikit-image',
-        'tables',
+        "scipy>=1.2.0",
+        "networkx",
+        "matplotlib",
+        "scikit-image",
+        "tables",
     ],
     packages=find_packages(),
 )
