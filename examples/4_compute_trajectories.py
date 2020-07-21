48c8f675cdc7cf36f4157645444d94a0bc06e8b1
saxedh <dhruv@mathphys01.ma.ic.ac.uk>
2020-07-21 11:22:29 +0100

    added tree and updated SM_small parameters

--- examples/4_compute_trajectories.py
+++ examples/4_compute_trajectories.py
@@ -23,12 +23,12 @@ graph = netsalt.load_graph()
 modes_df = netsalt.load_modes()
 
 # LOAD FROM FILE
-graph.graph['params']["pump"] = pickle.load(open("optimal_pump.pkl", "rb"))
+# graph.graph['params']["pump"] = pickle.load(open("optimal_pump.pkl", "rb"))
 # graph.graph['params']["pump"] = pickle.load(open("pump_missing_edge60.pkl", "rb"))
 
 # OR GENERATE PUMP PROFILE BASED ON PARAMS["PUMP_EDGES"]
-#generate_pump(graph_tpe, graph, params)
-#graph.graph["params"]["pump"] = params["pump"]
+generate_pump(graph_tpe, graph, params)
+graph.graph["params"]["pump"] = params["pump"]
 
 netsalt.update_parameters(graph, params)
 netsalt.save_graph(graph)
@@ -38,7 +38,7 @@ plotting.plot_quantum_graph(
     graph, edge_colors=graph.graph["params"]["pump"], node_size=0.1, save_option=False
 )
 plt.savefig("pump_profile.svg")
-plt.show()
+#plt.show()
 
 modes_df = netsalt.pump_trajectories(modes_df, graph, return_approx=True)
 netsalt.save_modes(modes_df)
@@ -48,4 +48,4 @@ qualities = netsalt.load_qualities()
 ax = plotting.plot_scan(
     graph, qualities, modes_df, filename="scan_with_trajectories", relax_upper=True
 )
-plt.show()
+#plt.show()
