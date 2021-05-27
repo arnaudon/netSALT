1c91d51e13ec0f72d04b4b7c8dc62cf4caa529f8
arnaudon <alexis.arnaudon@epfl.ch>
2021-05-27 21:32:07 +0200

    buffon missing pixel

---
+++ example_luigi/buffon/buffon_missing_pixel/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export OMP_NUM_THREADS=1
+export NUMEXPR_MAX_THREADS=1
+
+mkdir -p out
+mkdir -p figures
+
+python create_pump.py
+
+luigi --module netsalt.tasks ComputeLasingModes --local-scheduler --log-level INFO # --rerun #--rerun-all
+
