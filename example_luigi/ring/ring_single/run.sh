30c7f4623866e83ed992839bf6fade3b7c7234f8
arnaudon <alexis.arnaudon@epfl.ch>
2021-07-08 14:55:16 +0200

    fix ring_single

--- example_luigi/ring/ring_single/run.sh
+++ example_luigi/ring/ring_single/run.sh
@@ -8,5 +8,5 @@ mkdir -p figures
 python create_graph.py
 
 #luigi --module netsalt.tasks ComputePassiveModes --local-scheduler --log-level INFO --rerun
-luigi --module netsalt.tasks ComputeLasingModes --local-scheduler --log-level INFO  --rerun 
+luigi --module netsalt.tasks.workflow ComputeLasingModes --local-scheduler --log-level INFO  --rerun 
 
